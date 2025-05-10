095bcc19197cf2ac44acd518340167c569d34a85
unknown <zsuzska.nemecz@gmail.com>
2025-05-10 16:40:18 +0200

    data

--- part1_part2_lastrun.py
+++ part1_part2_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on Mai 10, 2025, at 10:40
+    on Mai 10, 2025, at 14:40
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
--- part3_lastrun.py
+++ part3_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on Mai 10, 2025, at 11:31
+    on Mai 10, 2025, at 15:12
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -1924,7 +1924,7 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
                 # Run 'Begin Routine' code from select_item
                 run_index = run_counter - 1
                 if run_counter > 4:
-                    run_index = run_counter - 1
+                    run_index = run_counter - 5
                 this_run = my_runs[run_index]
                 this_item = this_run.pop()
                 this_iti = iti_list.pop()
