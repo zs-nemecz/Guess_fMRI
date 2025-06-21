32260382e1ba3583dadb4c69bbb60d428bf00b2a
unknown <zsuzska.nemecz@gmail.com>
2025-06-21 12:13:05 +0200

    data

--- part0_DE_lastrun.py
+++ part0_DE_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on Juni 20, 2025, at 10:58
+    on Juni 21, 2025, at 10:09
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
