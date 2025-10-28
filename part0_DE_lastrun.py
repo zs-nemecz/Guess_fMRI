8c9041a26a46fe90180bd4ff79e453f264e6e11e
unknown <zsuzska.nemecz@gmail.com>
2025-10-28 16:30:44 +0100

    data

--- part0_DE_lastrun.py
+++ part0_DE_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on Oktober 28, 2025, at 12:56
+    on Oktober 28, 2025, at 15:34
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
--- part1_part2_lastrun.py
+++ part1_part2_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on Oktober 28, 2025, at 13:21
+    on Oktober 28, 2025, at 15:45
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
